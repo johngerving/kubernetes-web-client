67b49c8460e83d221143fa44ecd086f58ba6f89b
jhg51 <jhg51@humboldt.edu>
2024-10-10 08:33:16 -0700

    Initial commit, added frontend

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+
+<main>
+    <div class="w-screen h-screen flex items-center justify-center">
+        <form>
+            <button class="bg-blue-500 text-xl py-3 px-5 rounded-lg text-white shadow-md hover:bg-blue-600 transition-colors duration-300">Sign in</button>
+        </form>
+    </div>
+</main>
