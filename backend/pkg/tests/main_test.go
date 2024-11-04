0b1da20be73b62cf7a58c34c2e44af0620c66484
johngerving <gervingjohn@gmail.com>
2024-11-04 11:14:52 -0800

    Finished health check test in Go

--- backend/pkg/tests/main_test.go
+++ backend/pkg/tests/main_test.go
@@ -7,11 +7,14 @@ import (
 	"os"
 	"testing"
 
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGETHealth(t *testing.T) {
 	t.Run("returns API health status", func(t *testing.T) {
+		godotenv.Load(".backend.env")
+
 		port := os.Getenv("PORT")
 		resp, err := http.Get("http://localhost:" + port + "/health")
 
@@ -33,6 +36,19 @@ func TestGETHealth(t *testing.T) {
 			t.Fatalf("unable to unmarshal request response: %v", err)
 		}
 
-		t.Log(data)
+		require.Equal(t, data["status"], "up", "The API status should be 'up.'")
+
+		var details map[string]interface{}
+		require.IsType(t, data["details"], details, "The 'details' field should be a map[string]interface{}.")
+		details = data["details"].(map[string]interface{})
+
+		var database map[string]interface{}
+		require.IsType(t, details["database"], database, "The 'details['database']' field should be a map[string]interface{}.")
+		database = details["database"].(map[string]interface{})
+
+		var databaseType string
+		require.IsType(t, database["status"], databaseType, "The database status should be a string.")
+
+		require.Equal(t, database["status"], "up", "The databse status should be 'up.'")
 	})
 }
