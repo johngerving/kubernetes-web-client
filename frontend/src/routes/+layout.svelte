8a7ae9ef41470eec2594ac257c4c5ad4b645597b
johngerving <gervingjohn@gmail.com>
2024-11-20 10:53:02 -0800

    Fix services and ingress, add fetch hook to frontend

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,29 @@
-<script>
+<script lang="ts">
     import "../app.css";
+    import * as Avatar from "$lib/components/ui/avatar";
+
     /**
      * @typedef {Object} Props
      * @property {import('svelte').Snippet} [children]
      */
 
     /** @type {Props} */
-    let { children } = $props();
+    /** @type {{ data: import('./$types').LayoutData, children: Snippet }}*/
+    let { data, children } = $props();
+
+    const user = data.user as User;
 </script>
 
-{@render children?.()}
+<nav class="flex justify-end p-2 bg-slate-100">
+    <p>{user.email}</p>
+    <button>
+        <Avatar.Root class="">
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+        </Avatar.Root>
+    </button>
+</nav>
+
+<main>
+    {@render children?.()}
+</main>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-full px-56 py-8">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
